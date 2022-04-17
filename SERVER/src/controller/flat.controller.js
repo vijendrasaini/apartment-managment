a12638836cb48bb8a9bbd8576beef3ad6570df7c
Vijendra <docs.vijendra@gmail.com>
2022-04-17 13:05:52 +0530

    pagination is done

--- SERVER/src/controller/flat.controller.js
+++ SERVER/src/controller/flat.controller.js
@@ -20,24 +20,29 @@ router.post('/flat', async(req, res)=>{
 
 router.get('/flats', async (req, res)=>{
     try {
-            
-            const filter = {}
-            let flats = await Flat.find().lean().exec()
-            
-            // flats = req?.query?.sort &&  req.query.sort == 1 
-            if(req.query.q == 'sort'){
-                flats = req.query.sort == 1 ? flats.sort((a,b)=>(a.no - b.no)) : flats.sort((a,b)=>(-a.no + b.no))
-                // http://localhost:7000/flats?q=sort&sort=-1
-            }
-            else if(req.query.q == 'filter'){
-                // const filterReason = req.query.base
-                console.log({ query : req.query})
-                flats = flats.filter(flat => flat.type == req.query.base)
+            const page = req.query.page || 1
+            const limit = req.query.limit || 2
+            let flats = await Flat.find().skip((page - 1) * limit).limit(limit).lean().exec()
+            const totalDocs = await Flat.find().countDocuments()
+            const totolPages = (Math.ceil(totalDocs/limit))
+            if(req.query.q){
+                if(req.query.q == 'sort'){
+                    flats = req.query.sort == 1 ? flats.sort((a,b)=>(a.no - b.no)) : flats.sort((a,b)=>(-a.no + b.no))
+                    // http://localhost:7000/flats?q=sort&sort=-1
+                }
+                else if(req.query.q == 'filter'){
+                    flats = flats.filter(flat => flat.type == req.query.base)
+                    // http://localhost:7000/flats?q=filter&base=owner
+                }
+                else if(req.query.q == "search"){
+                    flats = flats.filter(flat => flat.block == req.query.block)
+                    // http://localhost:7000/flats?q=search&block=B
+                }
             }
 
             return res
             .status(200)
-            .send(flats)
+            .send({ flats, totolPages})
     } catch (error) {
         return res
         .status(500)

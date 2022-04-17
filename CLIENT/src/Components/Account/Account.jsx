1ac8dea64e7866af3f924e38f0453083d4352616
Vijendra <docs.vijendra@gmail.com>
2022-04-17 14:35:52 +0530

    login page layout is ready to use

--- CLIENT/src/Components/Account/Account.jsx
+++ CLIENT/src/Components/Account/Account.jsx
@@ -1,22 +1,53 @@
-
+import './account.css'
 
 export const Account = () => {
 
     return (
         <>
-            <h1>Resister</h1>
-            <div class="mb-3 row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"/>
+            <h1 className="mt-5">Resister</h1>
+
+            <div className='container my-container'>
+                <div className="split left">
+                    <div className="centered">
+                        <div className="container mt-5">
+                            <div className="mb-3 row">
+                                <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
+                                <div className="col-sm-10">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="mt-5 row">
+                                <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                                <div className="col-sm-10">
+                                    <input type="password" className="form-control" id="inputPassword" />
+                                </div>
+                            </div>
+                            <button className="btn btn-primary mt-5" type="button">SIGN UP</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="split right">
+                    <div className="centered">
+                        <div className="container mt-5">
+                            <div className="mb-3 row">
+                                <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
+                                <div className="col-sm-10">
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="mt-5 row">
+                                <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                                <div className="col-sm-10">
+                                    <input type="password" className="form-control" id="inputPassword" />
+                                </div>
+                            </div>
+                            <button className="btn btn-primary mt-5" type="button">SIGN IN</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="mb-3 row">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword"/>
-                </div>
-            </div>
+
         </>
     )
 }

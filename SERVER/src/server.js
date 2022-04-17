2f3ab9f92c958c52e97940833545871deacd04a8
Vijendra <docs.vijendra@gmail.com>
2022-04-17 11:44:28 +0530

    basic strcture is done for server configuration

--- SERVER/src/server.js
+++ SERVER/src/server.js
@@ -1,4 +1,19 @@
 const express = require('express')
 
-
+const mongoConnector = require('./configs/database')
 const app = express()
+const port = process.env.PORT || 7000
+
+module.exports = ()=>{
+    app.listen(port, async ()=>{
+        try {
+            await mongoConnector()
+            console.log(`Server is listening on the port ${port} ...`)    
+        } catch (error) {
+            console.log({
+                message : error.message,
+                status : "something bad happened"
+            })
+        }
+    } )
+}

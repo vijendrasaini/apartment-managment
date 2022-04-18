504698c21a61b4cc033a8cb59b0bd2910d59eae0
Vijendra <docs.vijendra@gmail.com>
2022-04-18 09:04:11 +0530

    auth and loding cases are handled now.

--- CLIENT/src/Redux/reducer.js
+++ CLIENT/src/Redux/reducer.js
@@ -1,13 +1,22 @@
 import { fetchData } from "./action";
-import { FETCH_DATA } from "./actionTypes";
+import { AUTH, FETCH_DATA, FETCH_RESIDENTS, LODING } from "./actionTypes";
 
 const initialState = {
-    dataObj : { }
+    dataObj : { },
+    residents : [],
+    loding : true,
+    auth : false
 }
 export const reducer = ( state = initialState, { type , payload})=>{
     switch(type){
         case FETCH_DATA:
             return { ...state, dataObj : payload}
+        case LODING:
+            return { ...state, loding : payload}
+        case FETCH_RESIDENTS:
+            return { ...state, residents : payload}
+        case AUTH : 
+            return { ...state, auth : payload}
         default : 
             return state
     }

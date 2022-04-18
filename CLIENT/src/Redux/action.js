29a205f5bf636119cb608f4e861946e520bcf66b
Vijendra <docs.vijendra@gmail.com>
2022-04-18 06:34:21 +0530

    fetch is fixed.

--- CLIENT/src/Redux/action.js
+++ CLIENT/src/Redux/action.js
@@ -6,7 +6,15 @@ import { FETCH_DATA } from "./actionTypes"
 export const setData = (payload)=>({ type : FETCH_DATA, payload})
 
 export const fetchData = (url) => async (dispatch)=>{
-    const res = await axios.get(url)
-    const data = res.data
+    const token = JSON.parse(localStorage.getItem('token'))
+    const a = await fetch(url, {
+        method : "GET",
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    })
+    const res = await a.json()
+    console.log({res})
+    const data = res
     dispatch(setData(data))
 }
